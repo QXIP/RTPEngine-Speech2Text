1d121468196aa6ad86c42dd429c2894dab510c90
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-10-11 14:35:13 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  hep_config: {
+    debug: true,
+    HEP_SERVER: '127.0.0.1',
+    HEP_PORT: 9060
+  },
+  bing_options: {
+    language: 'en-US',
+    subscriptionKey: 'YOUR-KEY-HERE' // https://azure.microsoft.com/en-us/services/cognitive-services
+  }
+}
+
+module.exports = config;
