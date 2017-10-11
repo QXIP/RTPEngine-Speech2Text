558fc9763135ab3180b550cff6e41d836ef07e92
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-10-11 14:32:08 +0200

    Create speech2hep.js

---
+++ speech2hep.js
@@ -0,0 +1,86 @@
+const chokidar = require('chokidar');
+const fs = require('fs');
+const speechService = require('ms-bing-speech-service');
+
+var config = require('./config.js');
+if (config.hep_config) {
+  console.log('Enabling HEP...');
+  var hep_client = require('./hep.js');
+  hep_client.init(config.hep_config);
+} 
+
+const options = config.bing_options; 
+const socket = new speechService(options);
+const watcher = chokidar.watch('/recording', {ignored: /^\./, persistent: true });
+watcher
+    .on('error', function(error) {console.error('Error happened', error);})
+    .on('add', function(path) {console.log('File', path, 'has been added');  })
+    // .on('change', function(path) {console.log('File', path, 'has been changed'); })
+    .on('unlink', function(path) {console.log('File', path, 'has been removed');
+	   if(path.endsWith('.meta')){ 
+		var newpath = path.replace(/\.meta/i, '-mix.wav');
+		try { var xcid = path.match(/\/([^\/]+)\/?\.meta$/)[1]; } catch(e) { console.log(e); }
+		// Get file timestamp, detection is delayed
+		var stats = fs.statSync(newpath);
+		var datenow = stats.mtime ? new Date(stats.mtime).getTime() : new Date().getTime();
+		var t_sec = Math.floor( datenow / 1000);
+		var u_sec = ( datenow - (t_sec*1000))*1000;
+
+		console.log('Meta Hit! Seeking Audio at: ',newpath);
+		console.log('CHECK!',hep_client,config);
+		socket.start((error, service) => {
+		  console.log('service started');
+		  service.on('recognition', (e) => {
+		    if (e.RecognitionStatus === 'Success') {
+          console.log('Response',e);
+          if (hep_client){
+            console.log('Sending HEP...');
+            try {
+            var message = {
+                    rcinfo: {
+                      type: 'HEP',
+                      version: 3,
+                      payload_type: 100,
+                      time_sec: t_sec,
+                      time_usec: u_sec,
+                      ip_family: 2,
+                      protocol: 17,
+                      proto_type: 100,
+                      srcIp: '127.0.0.1',
+                      dstIp: '127.0.0.1',
+                      srcPort: 0,
+                      dstPort: 0,
+                      captureId: 2999,
+                      capturePass: 'SPEECH-TO-HEP',
+                      correlation_id: xcid
+                    },
+                      payload: e.DisplayText
+                  };
+            hep_client.preHep(message);
+            } catch(e) { console.log(e) }
+          }
+		    }
+		  });
+
+		  service.sendFile(newpath, function(e){
+				setTimeout(function(){ fs.unlink(newpath); }, 1000);
+		  });
+
+		});
+	   }
+    })
+
+var exit = false;
+process.on('SIGINT', function() {
+  console.log();
+  if (exit) {
+    console.log("Exiting...");
+    process.exit();
+  } else {
+    console.log("Press CTRL-C within 2 seconds to Exit...");
+    exit = true;
+    setTimeout(function () {
+      exit = false;
+    }, 2000)
+  }
+});
